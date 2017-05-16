5b747bcbd2a9aafc9d9081b3dbc27add9e24731d
Ryzzan Abbade Salman <rabbadesalman@gmail.com>
2017-05-16 00:16:15 -0300

    profile menu

--- src/app/shared/components/menu-current-module/menu-current-module.component.ts
+++ src/app/shared/components/menu-current-module/menu-current-module.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MdSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+
+/*Services*/
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'menu-current-module',
@@ -6,11 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-current-module.component.css']
 })
 export class MenuCurrentModuleComponent implements OnInit {
+  public msg: any;
   public sidenav: boolean;
-  constructor() { }
+
+  constructor(
+    private authentication: AuthenticationService, 
+    public snackBar: MdSnackBar,
+    private router: Router
+  ) {
+
+  }
 
   ngOnInit() {
     this.sidenav = false;
+  }
+
+  logout = () => {
+    this.authentication.logout()
+    .then(res => {
+      this.msg = res;
+
+      this.snackBar.open(this.msg.message, '', {
+        duration: 2000,
+      });
+
+      if(this.msg.cod == "lo-01") {
+        this.router.navigate(['/login']);
+      }
+    })
+    .catch(rej => this.msg = [rej]);;
   }
 
   toggleSidenav = () => {
