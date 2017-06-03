feffb4ec5598731660cd22afb9cdeaba2c4c6fcf
Ryzzan Abbade Salman <rabbadesalman@gmail.com>
2017-06-03 12:38:15 -0300

    Signup

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+import { AuthenticationService } from './../../../../shared/services/authentication.service';
+import { CrudService } from './../../../../shared/services/crud.service';
+import { initializeApp } from 'firebase';
+import { firebaseConfig } from './../../../../../environments/firebase.config';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-  constructor() { }
+  msg: any;
+
+  constructor(private authentication: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout = () => {
+    this.authentication.logout()
+    .then(res => {
+      this.msg = res;
+
+      if(this.msg.cod == "lo-01") {
+        this.router.navigate(['/login']);
+      }
+    })
+    .catch(rej => this.msg = [rej]);;
   }
 }
